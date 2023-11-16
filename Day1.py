baa6fe0c8af20d7fbd8ded69c3d47853e11afd33
mdk44 <matt.karam@gmail.com>
2023-11-16 11:16:16 -0600

    Part 1 complete

--- Day1.py
+++ Day1.py
@@ -1,7 +1,16 @@
-# input_file = 'Day1_Input.csv'
-input_file = 'Day1_Test.csv'
+input_file = 'Day1_Input.csv'
+# input_file = 'Day1_Test.csv'
 text_file = open(input_file)
 lines = text_file.read().split('\n')
 
-for line in lines:
-    print(line)
+# Part 1
+i = 0
+cals = []
+cals.append(0)
+for x in range(0, len(lines)):
+    if lines[x] == "":
+        i += 1
+        cals.append(0)
+    else:
+        cals[i] += int(lines[x])
+print("Part 1: " + str(max(cals)))
