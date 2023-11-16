8af9a5cffa14aceba43d333c5118a170d38ac46e
mdk44 <matt.karam@gmail.com>
2023-11-16 12:01:09 -0600

    Part 2 complete!

--- Day2.py
+++ Day2.py
@@ -3,14 +3,11 @@ input_file = 'Day2_Input.csv'
 text_file = open(input_file)
 lines = text_file.read().split('\n')
 
-# A = Rock
-# B = Paper
-# C = Scissors
+# Part 1
 
-# X = Rock, adds 1 point
-# Y = Paper, adds 2 points
-# Z = Scissors, adds 3 points
-
+# A = Rock, B = Paper, C = Scissors
+# X = Rock, adds 1 point; Y = Paper, adds 2 points; Z = Scissors, adds 3 points
+# Lose = 0 points, Draw = 3 points, Win = 6 points
 scores = {}
 scores["A", "X"] = 4
 scores["A", "Y"] = 8
@@ -27,3 +24,25 @@ for line in lines:
     total_score += scores[line[0], line[2]]
 
 print("Part 1: " + str(total_score))
+
+# Part 2
+
+# ABC same as above
+# X = lose, Y = draw, Z = win, my choice adds same points as above
+# Same win points as above
+scores = {}
+scores["A", "X"] = 3
+scores["A", "Y"] = 4
+scores["A", "Z"] = 8
+scores["B", "X"] = 1
+scores["B", "Y"] = 5
+scores["B", "Z"] = 9
+scores["C", "X"] = 2
+scores["C", "Y"] = 6
+scores["C", "Z"] = 7
+
+total_score = 0
+for line in lines:
+    total_score += scores[line[0], line[2]]
+
+print("Part 2: " + str(total_score))
